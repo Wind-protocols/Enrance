db8f549eb49945bfd83d9c06170f2515ca5ca826
WindExperiment <mudenglab@gmail.com>
2022-03-17 21:49:13 -0700

    First commit

---
+++ typings.d.ts
@@ -0,0 +1,24 @@
+declare module 'slash2';
+declare module '*.css';
+declare module '*.less';
+declare module '*.scss';
+declare module '*.sass';
+declare module '*.svg';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.bmp';
+declare module '*.tiff';
+declare module 'mockjs';
+declare module '*.avi';
+declare module '*.tsx';
+declare module '*.svg' {
+  export function ReactComponent(props: React.SVGProps<SVGSVGElement>): React.ReactElement;
+  const url: string;
+  export default url;
+}
+declare module 'omit.js';
+declare module 'uuid';
+
+declare const REACT_APP_ENV: 'dev' | 'prd' | false;
